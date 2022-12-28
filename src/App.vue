704e67bfe2deedd66a22c211143069d5bd2bfee6
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2022-12-28 11:52:32 +0100

    Clean up code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';</script>
 
 <template>
   <header>
