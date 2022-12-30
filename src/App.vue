86511858585f94a42f1348b84867d762c2e74733
Anton Christensen <anton.christensen9700@gmail.com>
2022-12-30 18:46:10 +0100

    initial fetch orders code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
-<script setup lang="ts">
+<script lang="ts">
+  export default {
+
+    data() {
+      return {
+        lines: [] as string[]
+      }
+    },
+    methods: {
+      refetch() {
+        fetch("https://shopify-data.it.fantastiskefroe.dk/orders").then(
+          data => {
+            console.log(data);
+          }
+        ).catch(err => this.lines = ["foo"])
+      }
+    },
+    mounted() {
+      // methods can be called in lifecycle hooks, or other methods!
+      this.refetch()
+    }
+  }
+
 </script>
 
 <template>
   <div class="container">
     <div class="row my-5">
       <h1 class="display-4">Committed stock</h1>
+      <button class="btn-primary" @click="refetch()"></button>
     </div>
 
     <div class="row mb-3 justify-content-center">
