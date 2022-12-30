9095965f4c0934d830d4ed67f0426dde6e8580af
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2022-12-30 18:16:11 +0100

    Add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';</script>
+</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="row my-5">
+      <h1 class="display-4">Committed stock</h1>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="row mb-3 justify-content-center">
+      <ul>
+        <li>Stock</li>
+        <li>Stock</li>
+        <li>Stock</li>
+        <li>Stock</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-
-import './assets/main.css';
+import '@/assets/style.scss';
 
 createApp(App).mount('#app');
