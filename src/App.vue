ce63d432f94853f675720496e52033b43aef1c4e
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2022-12-31 17:05:10 +0100

    Bigger title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row my-3">
-      <h1>Solgt Lager</h1>
+      <h1 class="display-4">Solgt Lager</h1>
     </div>
 
     <div class="row mb-3 justify-content-center">
