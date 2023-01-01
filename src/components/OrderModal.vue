a850045f7aaf8f93f525b9f6f0b6e31876bfde56
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-01-01 15:00:35 +0100

    Remove footer from modal

--- src/components/OrderModal.vue
+++ src/components/OrderModal.vue
@@ -28,10 +28,6 @@
             </li>
           </ul>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
       </div>
     </div>
   </div>
